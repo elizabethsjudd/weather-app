8c754d535ce501aa578330ca599bb6c5de4afa93
Elizabeth (Judd) Byrne <esbyrne624@gmail.com>
2023-04-27 10:09:44 -0400

    test: more robust eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier", "eslint-plugin-tsdoc"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/strict",
+    "next",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "tsdoc/syntax": "error",
+  },
+};
