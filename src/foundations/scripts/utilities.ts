29d2868c94d760846672cf0fd90787d2643b6cac
Elizabeth (Judd) Byrne <esbyrne624@gmail.com>
2023-04-29 10:02:14 -0400

    fix(combineAttributes): remove undefined classes

--- src/foundations/scripts/utilities.ts
+++ src/foundations/scripts/utilities.ts
@@ -15,13 +15,15 @@ export const combineAttributes = (
 	attributes: AttrsObject,
 	defaults: AttrsObject = {}
 ): AttrsObject => {
+	// For classes, we want to combine the two values
+	let className: boolean | string = defaults.className && attributes.className ? `${defaults.className} ${attributes.className}` : false;
+
 	/**
 	 * Assign the defaults and the passed attributes to a new object
 	 * to ensure that one attributes object without duplicated keys
 	 * can be passed to the component.
 	 */
 	return Object.assign({}, defaults, attributes, {
-		// For classes, we want to combine the two values
-		className: `${defaults.className} ${attributes.className}`,
+		...(className && { className }),
 	});
 };
