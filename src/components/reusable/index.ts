64e640dc1f60304e5815deb34be9277a9e81718e
Elizabeth (Judd) Byrne <esbyrne624@gmail.com>
2023-04-29 13:01:14 -0400

    feat: create index file for reusable components

---
+++ src/components/reusable/index.ts
@@ -0,0 +1,7 @@
+export { Button } from "./button";
+export { Card } from "./card";
+export { Heading } from "./heading";
+export { Input } from "./input";
+export { Label } from "./label";
+export { Select } from "./select";
+export { SelectOption } from "./select-option";
