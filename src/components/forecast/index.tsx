943988ee08432c43053c6a9464419233a2680fd2
Elizabeth (Judd) Byrne <esbyrne624@gmail.com>
2023-05-03 10:58:10 -0400

    feat(forecast): allow user to change temperature units

--- src/components/forecast/index.tsx
+++ src/components/forecast/index.tsx
@@ -16,68 +16,93 @@ import styles from "./forecast.module.scss";
  * Component that uses the weather.gov API to pull the forecast for a given location
  * --------------------------------------------------
  */
-export const Forecast = ({ coordinates, hookUpdate, testId }: ForecastConfig): JSX.Element => {
+export const Forecast = ({
+	coordinates,
+	hookUpdateLocation,
+	testId,
+}: ForecastConfig): JSX.Element => {
 	const defaultWeatherArray: Array<DailyWeatherConfigConstructor> = [];
 	const defaultAPIError = "";
 
 	const [futureForecast, setFutureForecast] = React.useState(defaultWeatherArray);
 	const [currentWeather, setCurrentWeather] = React.useState(defaultWeatherArray);
+	const [forecastURL, setForecastURL] = React.useState("");
+	const [temperatureUnit, setTemperatureUnit] = React.useState("us");
 	const [apiError, setAPIError] = React.useState(defaultAPIError);
 
+	const changeUnit = (event: React.ChangeEvent) => {
+		setTemperatureUnit((event?.target as HTMLInputElement).checked ? "us" : "si");
+	};
+
+	// Watch for a forecast change
 	React.useEffect(() => {
-		if (coordinates.y !== 0 && coordinates.x !== 0) {
-			getLocationData(coordinates, async (locationData) => {
+		const requestURL = forecastURL !== "" ? `${forecastURL}?units=${temperatureUnit}` : "";
+
+		if (requestURL !== "") {
+			getForecast(requestURL, async (forecastData) => {
+				setCurrentWeather([]);
+				setFutureForecast([]);
+
+				let dayObject = new Object({}) as DailyWeatherConfigConstructor;
+				if (!(forecastData as WeatherGovForecastInfo).status) {
+					(forecastData as WeatherGovForecastInfo).properties.periods.forEach((period) => {
+						const currentPeriod = period.name.toLowerCase().includes("night") ? "night" : "day";
+
+						dayObject[currentPeriod] = {
+							dayOfTheWeek: period.name,
+							description: period.shortForecast,
+							temperature: period.temperature,
+							temperatureUnit: period.temperatureUnit,
+						};
+
+						if (currentPeriod === "night") {
+							const currentDayObject = Object.assign({}, dayObject);
+
+							if (isCurrentDay(period.name)) {
+								setCurrentWeather((prevArray) => [
+									...prevArray,
+									Object.assign({}, currentDayObject),
+								]);
+							} else {
+								setFutureForecast((prevArray) => [
+									...prevArray,
+									Object.assign({}, currentDayObject),
+								]);
+							}
+
+							dayObject = new Object({});
+						}
+					});
+				} else {
+					setAPIError(
+						`${
+							(forecastData as WeatherGovForecastInfo).status
+						} status error with forecast data base`
+					);
+				}
+			});
+		}
+	}, [temperatureUnit, forecastURL]);
+
+	// Watch for a Location change
+	React.useEffect(() => {
+		if (coordinates.x !== 0 && coordinates.y !== 0) {
+			getLocationData(coordinates, (locationData) => {
 				const locData = locationData as WeatherGovLocationInfo;
 				if (!locData.status) {
-					hookUpdate &&
-						hookUpdate({
+					hookUpdateLocation &&
+						hookUpdateLocation({
 							location: `${locData.properties.relativeLocation.properties.city}, ${locData.properties.relativeLocation.properties.state}`,
 						});
 
-					getForecast(locData.properties.forecast, (forecastData) => {
-						setCurrentWeather([]);
-						setFutureForecast([]);
-
-						let dayObject = new Object({}) as DailyWeatherConfigConstructor;
-						(forecastData as WeatherGovForecastInfo).properties.periods.forEach((period) => {
-							const currentPeriod = period.name.toLowerCase().includes("night") ? "night" : "day";
-
-							dayObject[currentPeriod] = {
-								dayOfTheWeek: period.name,
-								description: period.shortForecast,
-								temperature: period.temperature,
-								temperatureUnit: period.temperatureUnit,
-							};
-
-							if (currentPeriod === "night") {
-								const currentDayObject = Object.assign({}, dayObject);
-
-								if (isCurrentDay(period.name)) {
-									setCurrentWeather((prevArray) => [
-										...prevArray,
-										Object.assign({}, currentDayObject),
-									]);
-								} else {
-									setFutureForecast((prevArray) => [
-										...prevArray,
-										Object.assign({}, currentDayObject),
-									]);
-								}
-
-								dayObject = new Object({});
-							}
-						});
-					});
+					setForecastURL(locData.properties.forecast);
 				} else {
-					hookUpdate && hookUpdate({ location: "" });
+					hookUpdateLocation && hookUpdateLocation({ location: "" });
 					setAPIError(`${locData.status} status error with data base`);
 				}
 			});
-		} else {
-			hookUpdate && hookUpdate({ location: "" });
-			setAPIError("There was an issue connecting to the data base");
 		}
-	}, [coordinates, hookUpdate]);
+	}, [coordinates, hookUpdateLocation]);
 
 	const getWeatherCards = (weatherArray: Array<DailyWeatherConfig>, isCurrent = false) => {
 		const opAttrs: { testId?: string } = {};
@@ -108,6 +133,11 @@ export const Forecast = ({ coordinates, hookUpdate, testId }: ForecastConfig): J
 
 	return (
 		<>
+			{/* @todo - Long-term we'd want to store the user's preference  */}
+			<label>
+				Preferred Units:
+				<input defaultChecked={true} onChange={changeUnit} type="checkbox" value="us" />
+			</label>
 			{getWeatherCards(currentWeather as DailyWeatherConfig[], true)}
 			{futureForecast.length > 0 && (
 				<Heading kind="headline" level={3}>
