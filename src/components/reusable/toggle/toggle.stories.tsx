87f4eec1bd2d06af00144aeb413a4fc932da991f
Elizabeth (Judd) Byrne <esbyrne624@gmail.com>
2023-05-03 13:16:28 -0400

    docs: update toggle story setup

--- src/components/reusable/toggle/toggle.stories.tsx
+++ src/components/reusable/toggle/toggle.stories.tsx
@@ -1,20 +1,37 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Label } from "../label";
 import { Toggle } from "./index";
+import { ToggleConfig } from "./constants";
+import { action } from "@storybook/addon-actions";
 
-const meta: Meta<typeof Toggle> = {
-	component: Toggle,
+const meta: Meta<ToggleConfig["labels"]> = {
+	args: {
+		off: "Off",
+		on: "On",
+	},
+	argTypes: {
+		off: {
+			control: { type: "text" },
+		},
+		on: {
+			control: { type: "text" },
+		},
+	},
 	title: "Reusable/Toggle",
 };
 
 export default meta;
-type Story = StoryObj<typeof Toggle>;
+type Story = StoryObj<ToggleConfig["labels"]>;
 
 export const Default: Story = {
-	render: () => (
+	render: (args) => (
 		<>
 			<Label attrs={{ htmlFor: "meow" }}>Label</Label>
-			<Toggle attrs={{ defaultValue: "cat", id: "meow", name: "pet-selector" }}></Toggle>
+			<Toggle
+				attrs={{ id: "meow" }}
+				hookChange={action("toggle changed value")}
+				labels={{ off: args.off, on: args.on }}
+			></Toggle>
 		</>
 	),
 };
